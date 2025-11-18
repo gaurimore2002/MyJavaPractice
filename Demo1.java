ab1c986e15cee7b43b21c4f9ed88ebc688bf1fe5
gaurimore2002 <gaurimore35032@gmail.com>
2025-11-18 22:56:18 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String userName = name.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
+}
